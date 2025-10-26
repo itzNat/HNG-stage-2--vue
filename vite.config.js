e9729105fa8ce98335e1847d50f749d8cede9206
itzNat <legendaryelzio@gmail.com>
2025-10-26 18:55:24 +0100

    Added base path

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 })
